86466a51822744cb7009c2c404126e26b999182f
lee <leetgaines@gmail.com>
2025-05-05 09:27:47 -0400

    foundational -> foundation

--- infra/gcp/modules/foundation/main.tf
+++ infra/gcp/modules/foundation/main.tf
@@ -1,4 +1,4 @@
-// The foundational module contains things like cloud storage and networking.
+// The foundation module contains things like cloud storage and networking.
 // Put things here that probably don't change very often.
 // This module should be able to be used by things other than ML infra
 
